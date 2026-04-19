68caf6e64ff10641acce02423968d155b72edeca
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-04-19 23:38:48 +0100

    feat: update controller

--- src/main/java/com/dipen/k8shelmdeploy/controller/HomeController.java
+++ src/main/java/com/dipen/k8shelmdeploy/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String home(){
-        return "Welcome from home controller using helm!!!";
+        return "Welcome from home controller using helm!!!what's up";
     }
 }
